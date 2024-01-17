78038adc05ec9e0d88ebfaedfd9061fbb87cb7c6
Anthony Cueva <hi.cuevantn@gmail.com>
2024-01-17 17:35:52 -0500

    fix: rename trpc-server to server

--- apps/demo-app/src/app/api/trpc/[trpc]/route.ts
+++ apps/demo-app/src/app/api/trpc/[trpc]/route.ts
@@ -1,6 +1,6 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
-import { appRouter } from "@/trpc-server";
-import { createContext } from "@/trpc-server/context";
+import { appRouter } from "@/server";
+import { createContext } from "@/server/context";
 
 const handler = (req: Request) =>
   fetchRequestHandler({
--- apps/demo-app/src/lib/trpc.ts
+++ apps/demo-app/src/lib/trpc.ts
@@ -1,5 +1,5 @@
 import { createTRPCReact } from "@trpc/react-query";
-import { type AppRouter } from "@/trpc-server";
+import { type AppRouter } from "@/server";
 
 export const trpc: ReturnType<typeof createTRPCReact<AppRouter>> =
   createTRPCReact<AppRouter>({});
